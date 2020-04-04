c664f0ee19040c8c61258a1bbec0c07c64603ab0
Woo <lukasz.kwiek@gmail.com>
2020-04-04 15:50:15 +0200

    Update bme280.py
    Just fresh copy, due to problems with my raspberry ...

--- bme280/bme280.py
+++ bme280/bme280.py
@@ -1,10 +1,10 @@
 from enum import IntEnum
 
 class Device(IntEnum):
-    ADDRESS = 0x76
+    ADDRESS = 0x76  # ???
     ID = 0x60
 
-class Register(IntEnum):
+class Register(IntEnum):  
     ID = 0xd0
     RESET = 0xe0
     CTRL_HUM = 0xf2
@@ -33,30 +33,44 @@ class Ctrl_hum(IntEnum):
     OVERSAMPLING_16 = 0b101
 
 class Status(IntEnum):
-    MEASURING = 0b100
+    MEASURING = 0b1000
     IM_UPDATE = 0b001
 
-class Osrs_p(IntEnum):
-    offset = 3
-    SKIPPED = 0b000 << offset
-    OVERSAMPLING_1 = 0b001 << offset
-    OVERSAMPLING_2 = 0b010 << offset
-    OVERSAMPLING_4 = 0b011 << offset
-    OVERSAMPLING_8 = 0b100 << offset
-    OVERSAMPLING_16 = 0b101 << offset
-
-class Osrs_t(IntEnum):
-    offset = 6
-    SKIPPED = 0b000 << offset
-    OVERSAMPLING_1 = 0b001 << offset
-    OVERSAMPLING_2 = 0b010 << offset
-    OVERSAMPLING_4 = 0b011 << offset
-    OVERSAMPLING_8 = 0b100 << offset
-    OVERSAMPLING_16 = 0b101 << offset
-
-class Mode(IntEnum):
+class Ctrl_meas(IntEnum):
+    _osrs_p_offset = 3
+    _osrs_t_offset = 6
+    PRESSURE_SKIPPED = 0b000 << _osrs_p_offset
+    PRESSURE_OVERSAMPLING_1 = 0b001 << _osrs_p_offset
+    PRESSURE_OVERSAMPLING_2 = 0b010 << _osrs_p_offset
+    PRESSURE_OVERSAMPLING_4 = 0b011 << _osrs_p_offset
+    PRESSURE_OVERSAMPLING_8 = 0b100 << _osrs_p_offset
+    PRESSURE_OVERSAMPLING_16 = 0b101 << _osrs_p_offset
+    TEMPERATURE_SKIPPED = 0b000 << _osrs_t_offset
+    TEMPERATURE_OVERSAMPLING_1 = 0b001 << _osrs_t_offset
+    TEMPERATURE_OVERSAMPLING_2 = 0b010 << _osrs_t_offset
+    TEMPERATURE_OVERSAMPLING_4 = 0b011 << _osrs_t_offset
+    TEMPERATURE_OVERSAMPLING_8 = 0b100 << _osrs_t_offset
+    TEMPERATURE_OVERSAMPLING_16 = 0b101 << _osrs_t_offset
     SLEEP = 0b00
     FORCED = 0b01
     NORMAL = 0b11
 
+class Config(IntEnum):
+    _t_sb_offset = 6
+    _filter_offset = 2
+    STANDBY_0 = 0b000 << _t_sb_offset
+    STANDBY_62 = 0b001 << _t_sb_offset
+    STANDBY_125 = 0b010 << _t_sb_offset
+    STANDBY_250 = 0b011 << _t_sb_offset
+    STANDBY_500 = 0b100 << _t_sb_offset
+    STANDBY_1000 = 0b101 << _t_sb_offset
+    STANDBY_10 = 0b110 << _t_sb_offset
+    STANDBY_20 = 0b111 << _t_sb_offset
+    FILTER_OFF = 0b000 << _filter_offset
+    FILTER_2 = 0b001 << _filter_offset
+    FILTER_4 = 0b010 << _filter_offset
+    FILTER_8 = 0b011 << _filter_offset
+    FILTER_16 = 0b100 << _filter_offset
+    SPI_ENABLE = 0b1
+    SPI_DISABLE = 0b0
 
